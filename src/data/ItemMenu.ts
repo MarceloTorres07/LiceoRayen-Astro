fcc8cf7b440025dd4d9d13225246f8d06f7d2c4c
Marcelo Torres <marceloignacio.torres@alumnos.ulagos.cl>
2025-11-04 21:44:51 -0300

    1

--- src/data/ItemMenu.ts
+++ src/data/ItemMenu.ts
@@ -4,36 +4,36 @@ export const menuItems = [
     label: "INSTITUCIÓN",
     submenu: [
       { label: "Historia", href: "../nav/historia" },
-      { label: "Equipo Directivo", href: "/nav/equipo directivo.html" },
+      { label: "Equipo Directivo", href: "/nav/equipo directivo" },
     ],
   },
   {
     label: "ÁREA H.C",
     submenu: [
-      { label: "1° y 2° medio", href: "../nav/plan de estudio-1-.html" },
-      { label: "3° y 4° medio", href: "../nav/plan de estudio-2-.html" },
+      { label: "1° y 2° medio", href: "../nav/plan de estudio-1-" },
+      { label: "3° y 4° medio", href: "../nav/plan de estudio-2-" },
     ],
   },
   {
     label: "ÁREA TÉCNICA",
     submenu: [
-      { label: "Gastronomía", href: "../nav/gastronomia.html" },
-      { label: "Construcción", href: "../nav/construccion.html" },
+      { label: "Gastronomía", href: "../nav/gastronomia" },
+      { label: "Construcción", href: "../nav/construccion" },
     ],
   },
   {
     label: "TALLERES",
     submenu: [
       { label: "PTU", href: "../nav/PTU.html" },
-      { label: "Complementarios", href: "../nav/complementarios.html" },
-      { label: "Selecciones deportivas", href: "../nav/deportivos.html" },
+      { label: "Complementarios", href: "../nav/complementarios" },
+      { label: "Selecciones deportivas", href: "../nav/deportivos" },
     ],
   },
   {
     label: "ÁREA AMBIENTAL",
     submenu: [
-      { label: "Brigada Ecológica", href: "../nav/brigada ecologica.html" },
-      { label: "Reciclaje", href: "../nav/reciclaje.html" },
+      { label: "Brigada Ecológica", href: "../nav/brigada ecologica" },
+      { label: "Reciclaje", href: "../nav/reciclaje" },
     ],
   },
 ];
